b152418e71912ceed88006ad68ea33de7f5b4a8a
Zanna Fitch <zannafitch2004@gmail.com>
2023-06-13 10:21:44 -0500

    add tests for 2 methods

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Visitor do 
   describe "#intialize" do
-    it "exists and ahs attributes" do
+    it "exists and has attributes" do
       visitor1 = Visitor.new('Bruce', 54, '$10')
 
       expect(visitor1).to be_a Visitor 
@@ -11,6 +11,34 @@ RSpec.describe Visitor do
       expect(visitor1.height).to eq(54)
       expect(visitor1.spending_money).to eq(10)
       expect(visitor1.preferences).to eq([])
+    end
+  end
+
+  describe "#add_preference" do
+    it "can add preferences" do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+
+      visitor1.add_preference(:gentle)
+      visitor1.add_preference(:thrilling)
+
+      expect(visitor1.preferences).to eq([:gentle, :thrilling])
+    end
+  end
+
+  describe "#tall_enough?" do
+    it "can check if tall enough" do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$15')
+
+      visitor1.tall_enough?(54)
+      visitor2.tall_enough?(54)
+
+      expect(visitor1.tall_enough?(54)).to eq(true)
+      expect(visitor2.tall_enough?(54)).to eq(false)
+      expect(visitor3.tall_enough?(54)).to eq(true)
+      expect(visitor1.tall_enough?(64)).to eq(false)
+
     end
   end
 end
